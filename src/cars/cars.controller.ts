c5c6e368e089ba54e585e536814c49c99f2086a1
Sebastian <sebastian.sanhueza.t@usach.cl>
2026-04-23 14:12:20 -0400

    inyección de dependencias

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -1,23 +1,24 @@
 // Los Controller Controlan rutas, son los encargados de escuchar la solicitud y emitir una respuesta.
 
 import { Controller, Get, Param } from '@nestjs/common';
+import { CarsService } from './cars.service';
 
 @Controller('cars')
 export class CarsController {
+	constructor(private readonly carsService: CarsService) {}
+
 	@Get()
 	getAllCars() {
-		return this.cars;
+		return this.carsService.findAll();
 	}
 
 	@Get(':id')
 	GetCarById(@Param('id') id: string) {
-		let car = this.cars[+id];
+		// if (!car) {
+		// 	return { id: '?', car: 'Auto no encontrado' };
+		// }
 
-		if (!car) {
-			car = 'Auto no asignado';
-		}
-
-		console.log({ id, car });
-		return { id, car };
+		console.log({ id: +id, car: this.carsService.findOneById(+id) });
+		return this.carsService.findOneById(Number(id));
 	}
 }
